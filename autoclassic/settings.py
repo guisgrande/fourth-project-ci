69cce2feb61f39ccd582b0d891619328293e25db
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-06-29 08:13:04 +0000

    Add new import and update SECRET_KEY to be hiden

--- autoclassic/settings.py
+++ autoclassic/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i$ma-06kaj62cjsh3x#)&q@nojvqqf8bb9*c%#(i@b-@i@v%d%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
