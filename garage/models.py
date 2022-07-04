2d4d53d4566d3533dca7c7656f0b79d6e46fee97
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-04 08:57:55 +0000

    Add class for Car post database model

--- garage/models.py
+++ garage/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Car(models.Model):
+    """
+    Class to represent Car database model
+    """
+    car_id = models.SmallAutoField(primary_key=True)
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='car_post')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+    price = models.FloatField(max_length=11)
+    hp = models.FloatField(max_length=4)
+    speed = models.FloatField(max_length=6)
+    featured_image = CloudinaryField('image', default='placeholder')
+    favorite = models.ManyToManyField(
+        User, related_name='favorite_car',
+        blank=True)
+    slug = models.SlugField(unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        """
+        Class to show newest car first 
+        """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.car_id
+
+    def number_of_favorites(self):
+        """
+        Function to count number of favorites
+        """
+        return self.favorite.count()
