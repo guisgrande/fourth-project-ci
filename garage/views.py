3117ea69143559cfcfd45cdeb16f2f2823b7a5eb
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-05 08:38:47 +0000

    Add class to view Car List

--- garage/views.py
+++ garage/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Car
 
-# Create your views here.
+
+class CarList(generic.ListView):
+    model = Car
+    queryset = Car.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 4
