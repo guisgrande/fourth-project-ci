26deb41feeb04684d23a6ebaf54662b5ca027829
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-05 08:43:29 +0000

    Add path to garage and events urls

--- autoclassic/urls.py
+++ autoclassic/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('garage.urls'), name='garage_urls'),
+    path('', include('events.urls'), name='events_urls'),
 ]
