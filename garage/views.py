17adb4d6f7629905ec386bda199d8d5575ae562d
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-12 10:35:12 +0000

    Change class to IndexView, add two funtions

--- garage/views.py
+++ garage/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.views import generic, View
 from .models import Car
+from events.models import Event
 
-
-class CarList(generic.ListView):
-    model = Car
-    queryset = Car.objects.filter(status=1).order_by('-created_on')
+class IndexView(generic.ListView):
     template_name = 'index.html'
+    context_object_name = 'car_list'
     paginate_by = 4
+    
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context.update({
+            'event_list': Event.objects.order_by('-created_on'),
+            'more_context': Car.objects.all(),
+        })
+        return context
+
+    def get_queryset(self):
+        return Car.objects.order_by('-created_on')
 
 
 class GarageView(generic.TemplateView):
