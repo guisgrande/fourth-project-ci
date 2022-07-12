9b7f4dfc7182f7c64983c43300b9ea4a4ba4f752
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-12 09:35:18 +0000

    Add search, list display and filter, prepopulated slug field

--- events/admin.py
+++ events/admin.py
@@ -5,4 +5,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Event)
 class EventsAdmin(SummernoteModelAdmin):
+
+    list_display = ('username', 'slug', 'category', 'status', 'created_on')
+    search_fields = ['username', 'event_title', 'category']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('username', 'event_title',)}
     summernote_fields = ('description')
--- garage/admin.py
+++ garage/admin.py
@@ -5,4 +5,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Car)
 class GarageAdmin(SummernoteModelAdmin):
+
+    list_display = ('username', 'slug', 'brand', 'model', 'status', 'created_on')
+    search_fields = ['username', 'brand', 'model']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('username', 'model', 'year',)}
     summernote_fields = ('description')
