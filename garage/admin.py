d64a668bb797e91644ca648aa0e29763592b2633
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-12 08:50:26 +0000

    Add SummetnoteModelAdmin, decorator and class

--- garage/admin.py
+++ garage/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Car
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Car)
+
+@admin.register(Car)
+class GarageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description')
