b5c986ef0511de6d27b2cbba367e1e64c70e48bd
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-12 10:36:01 +0000

    Change class name at path to new one, IndexView

--- garage/urls.py
+++ garage/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.CarList.as_view(), name='home'),
+    path('', views.IndexView.as_view(), name='home'),
     path('garage/', views.GarageView.as_view(), name='garage'),
 ]
