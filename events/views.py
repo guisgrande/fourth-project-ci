f35f3daa5ffbf67b8b158751f119ac33d8be591e
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-12 08:19:33 +0000

    Comment out EventList class code

--- events/views.py
+++ events/views.py
@@ -3,11 +3,12 @@ from django.views import generic, View
 from .models import Event
 
 
-class EventList(generic.ListView):
-    model = Event
-    queryset = Event.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-    paginate_by = 4
+#class EventList(generic.ListView):
+#    model = Event
+#    queryset = Event.objects.filter(status=1).order_by('-created_on')
+#    template_name = 'events.index.html'
+#    paginate_by = 4
+
 
 class EventsView(generic.TemplateView):
     template_name = 'events.html'
