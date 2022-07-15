493c86cb97275df5776b39dc4afffb0d6cdf7027
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-15 10:41:46 +0000

    Add function to delete user

--- members/views.py
+++ members/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic, View
+from .forms import UserDeleteForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 class MemberArea(generic.TemplateView):
     template_name = 'members/user_area.html'
+
+
+@login_required
+def deleteuser(request):
+    if request.method == 'POST':
+        delete_form = UserDeleteForm(request.POST, instance=request.user)
+        user = request.user
+        user.delete()
+        messages.info(request, 'Your account has been deleted.')
+        return redirect('home')
+    else:
+        delete_form = UserDeleteForm(instance=request.user)
+
+    context = {
+        'delete_form': delete_form
+    }
+
+    return render(request, 'members/delete.html', context)
