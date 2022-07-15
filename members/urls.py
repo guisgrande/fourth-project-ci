e9cda65361f696fad0af52bc2e0fade724f5c3df
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-15 10:43:37 +0000

    Add delete path to urlpatterns, import deleteuser from views

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
+from members.views import deleteuser
 
 urlpatterns = [
     path('members/', views.MemberArea.as_view(), name='members'),
+    path('delete/', deleteuser, name='delete'),
 ]
