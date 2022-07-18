b2e0dbac04f0f2d6eb4e41a168d919acdeadf836
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-18 10:24:04 +0000

    Add path to add car views in urlpatterns

--- garage/urls.py
+++ garage/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from .views import AddCarPost
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('garage/', views.GarageView.as_view(), name='garage'),
+    path('addcar/', views.AddCarPost.as_view(), name='add_car'),
 ]
