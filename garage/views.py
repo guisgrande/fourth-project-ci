b46920ea67d1f0c8080be979f4787e095bdf5d91
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-18 10:22:51 +0000

    Add class to add new car post for user

--- garage/views.py
+++ garage/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views import generic, View
 from .models import Car
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 from events.models import Event
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 class IndexView(generic.ListView):
+    """
+    Class used to display index content list.
+    Show last cars and events at index page.
+    """
     template_name = 'index.html'
     context_object_name = 'car_list'
     paginate_by = 4
@@ -22,3 +31,45 @@ class IndexView(generic.ListView):
 
 class GarageView(generic.TemplateView):
     template_name = 'garage.html'
+
+
+class AddCarPost(SuccessMessageMixin, LoginRequiredMixin, generic.CreateView):
+    """
+    Logged in user can create a car post
+    """
+    model = Car
+    fields = [
+        'brand',
+        'model',
+        'year',
+        'price',
+        'hp',
+        'speed',
+        'description',
+        'featured_image',
+        ]
+    template_name = 'members/add_car.html'
+    success_url = reverse_lazy('members')
+    success_message = "You have added car to the garage!"
+
+    def form_valid(self, form):
+        """
+        Sets logged in user as username field in form
+        Sets form default status to published
+        """
+        form.instance.username = self.request.user
+        form.instance.status = 1
+        return super(AddCarPost, self).form_valid(form)
+
+#    def auto_slug(self, form):
+#        """
+#        Sets slug field to auto field
+#        """
+#
+#        first_field = self.request.user
+#        second_field = self.request.model
+#        third_field = self.request.year
+#        autofield = first_field + second_field + third_field
+#        
+#        form.instance.slug = autofield
+#        return super(AddCarPost, self).form_valid(form)
