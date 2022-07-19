31f891f7a8ff0a49a690d8376a57a6b1d134a907
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-19 07:19:07 +0000

    Remove IndexView class, to keep code organized

--- garage/views.py
+++ garage/views.py
@@ -1,32 +1,10 @@
 from django.shortcuts import render
 from django.views import generic, View
-from .models import Car
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-from events.models import Event
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-
-class IndexView(generic.ListView):
-    """
-    Class used to display index content list.
-    Show last cars and events at index page.
-    """
-    template_name = 'index.html'
-    context_object_name = 'car_list'
-    paginate_by = 4
-    
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        context.update({
-            'event_list': Event.objects.order_by('-created_on'),
-            'more_context': Car.objects.all(),
-        })
-        return context
-
-    def get_queryset(self):
-        return Car.objects.order_by('-created_on')
+from .models import Car
 
 
 class GarageView(generic.TemplateView):
