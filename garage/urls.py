2c20c23a143b67dd639a5cd3260348b5461ab429
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-19 07:19:56 +0000

    Remove path to IndexView class

--- garage/urls.py
+++ garage/urls.py
@@ -1,9 +1,8 @@
 from . import views
 from django.urls import path
-from .views import AddCarPost
+
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='home'),
     path('garage/', views.GarageView.as_view(), name='garage'),
     path('addcar/', views.AddCarPost.as_view(), name='add_car'),
 ]
