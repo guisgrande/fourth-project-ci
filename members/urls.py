860505e75439ec99abc952d36dc29c9a11abb51b
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-21 13:51:24 +0000

    Add path to membercars function

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
-from members.views import deleteuser
+from members.views import deleteuser, membercars
 
 urlpatterns = [
     path('members/', views.MemberArea.as_view(), name='members'),
+    path('membercars/', membercars, name='membercars'),
     path('delete/', deleteuser, name='delete'),
 ]
