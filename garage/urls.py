0c5b876daa3ba2779fe3741c3a351ee796b15b00
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-21 15:13:05 +0000

    Add path to CarDetail class view

--- garage/urls.py
+++ garage/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('garage/', views.GarageView.as_view(), name='garage'),
     path('addcar/', views.AddCarPost.as_view(), name='add_car'),
+    path('cardetail/<slug:slug>/', views.CarDetail.as_view(), name='car_detail'),
 ]
