03bb5cc9ef479fed24fee8700ff3bbe104487a50
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-22 14:41:42 +0000

    Edti EventsView class to display all events posts

--- events/views.py
+++ events/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from django.contrib.auth.models import User
 from .models import Event
 
 
-#class EventList(generic.ListView):
-#    model = Event
-#    queryset = Event.objects.filter(status=1).order_by('-created_on')
-#    template_name = 'events.index.html'
-#    paginate_by = 4
-
-
-class EventsView(generic.TemplateView):
+class EventsView(generic.ListView):
+    """
+    Class to display all event posts at events page.
+    Ordered by new one show first an paginated by 6.
+    """
+    model = Event
     template_name = 'events/events.html'
+    context_object_name = 'event_list'
+    queryset = Event.objects.filter(status=1).order_by('-created_on')
+    paginate_by = 6
