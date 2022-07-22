38ee96ad7eb11a23dcfb5ba4b0201e769404c89d
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-22 17:14:15 +0000

    Add path to AddEventPost view

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('events/', views.EventsView.as_view(), name='events'),
+    path('addevent/', views.AddEventPost.as_view(), name='add_event'),
     path('eventdetail/<slug:slug>/', views.EventDetail.as_view(), name='event_detail'),
 ]
