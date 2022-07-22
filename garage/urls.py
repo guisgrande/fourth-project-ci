044d01a561020f64cda6dfc238d28d92341d7b00
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-22 12:59:53 +0000

    Add path to DeleteCarPost view

--- garage/urls.py
+++ garage/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('garage/', views.GarageView.as_view(), name='garage'),
     path('addcar/', views.AddCarPost.as_view(), name='add_car'),
     path('editcar/<slug:slug>/', views.EditCarPost.as_view(), name='edit_car'),
+    path('deletecar/<slug:slug>/', views.DeleteCarPost.as_view(), name='delete_car'),
     path('cardetail/<slug:slug>/', views.CarDetail.as_view(), name='car_detail'),
 ]
