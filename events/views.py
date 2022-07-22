5be76d192fe28ac894f84b0b2e31a4b45b649cf9
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-22 17:12:26 +0000

    Add AddEventPost class view to logged in user create event post

--- events/views.py
+++ events/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.contrib.auth.models import User
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.urls import reverse_lazy
 from .models import Event
 
 
@@ -25,3 +29,30 @@ class EventDetail(View):
     
         return render(request, "events/event_details.html", {"event": event})
 
+
+class AddEventPost(SuccessMessageMixin, LoginRequiredMixin, generic.CreateView):
+    """
+    Logged in user can create a event post.
+    """
+    model = Event
+    fields = [
+        'event_title',
+        'category',
+        'start_date',
+        'start_time',
+        'description',
+        'local',
+        'event_image',
+        ]
+    template_name = 'events/add_event.html'
+    success_url = reverse_lazy('members')
+    success_message = "You event was added to Events!"
+
+    def form_valid(self, form):
+        """
+        Sets logged in user as username field in form
+        Sets form default status to published
+        """
+        form.instance.username = self.request.user
+        form.instance.status = 1
+        return super(AddEventPost, self).form_valid(form)
