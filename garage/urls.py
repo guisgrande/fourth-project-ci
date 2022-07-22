591df9cb09d14f9c7c5b517b1ac6b33b22366b4f
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-22 12:03:26 +0000

    Add path to EditCarPost class view

--- garage/urls.py
+++ garage/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('garage/', views.GarageView.as_view(), name='garage'),
     path('addcar/', views.AddCarPost.as_view(), name='add_car'),
+    path('editcar/<slug:slug>/', views.EditCarPost.as_view(), name='edit_car'),
     path('cardetail/<slug:slug>/', views.CarDetail.as_view(), name='car_detail'),
 ]
