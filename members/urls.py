9979b1861bbaa557c277d5f4b373adb5082e9a82
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-23 10:29:14 +0000

    Add path to membersevents function view

--- members/urls.py
+++ members/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
-from members.views import deleteuser, membercars
+from members.views import deleteuser, membercars, memberevents
 
 urlpatterns = [
     path('members/', views.MemberArea.as_view(), name='members'),
     path('membercars/', membercars, name='membercars'),
+    path('memberevents/', memberevents, name='memberevents'),
     path('delete/', deleteuser, name='delete'),
 ]
