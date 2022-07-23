db0d8822828ca9e10eb6e450b0f91e843ecd4e15
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-23 11:14:57 +0000

    Add path to DeleteEventPost class view

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('events/', views.EventsView.as_view(), name='events'),
-    path('addevent/', views.AddEventPost.as_view(), name='add_event'),
-    path('editevent/<slug:slug>/', views.EditEventPost.as_view(), name='edit_event'),
-    path('eventdetail/<slug:slug>/', views.EventDetail.as_view(), name='event_detail'),
+    path('add-event/', views.AddEventPost.as_view(), name='add_event'),
+    path('edit-event/<slug:slug>/', views.EditEventPost.as_view(), name='edit_event'),
+    path('delete-event/<slug:slug>/', views.DeleteEventPost.as_view(), name='delete_event'),
+    path('event-detail/<slug:slug>/', views.EventDetail.as_view(), name='event_detail'),
 ]
