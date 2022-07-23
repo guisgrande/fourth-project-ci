5f69f464060113751536bbc8e8e61ad71ca1b615
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-23 11:21:35 +0000

    Edit paths, add separator to urls

--- garage/urls.py
+++ garage/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('garage/', views.GarageView.as_view(), name='garage'),
-    path('addcar/', views.AddCarPost.as_view(), name='add_car'),
-    path('editcar/<slug:slug>/', views.EditCarPost.as_view(), name='edit_car'),
-    path('deletecar/<slug:slug>/', views.DeleteCarPost.as_view(), name='delete_car'),
-    path('cardetail/<slug:slug>/', views.CarDetail.as_view(), name='car_detail'),
+    path('add-car/', views.AddCarPost.as_view(), name='add_car'),
+    path('edit-car/<slug:slug>/', views.EditCarPost.as_view(), name='edit_car'),
+    path('delete-car/<slug:slug>/', views.DeleteCarPost.as_view(), name='delete_car'),
+    path('car-detail/<slug:slug>/', views.CarDetail.as_view(), name='car_detail'),
 ]
