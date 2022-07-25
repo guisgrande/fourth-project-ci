38da2b5748fb8ffeb69697c2edd62ca34057a769
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-25 21:05:16 +0000

    Add PresenceGoEvent and PresenceMaybeEvent class view path

--- events/urls.py
+++ events/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('edit-event/<slug:slug>/', views.EditEventPost.as_view(), name='edit_event'),
     path('delete-event/<slug:slug>/', views.DeleteEventPost.as_view(), name='delete_event'),
     path('event-detail/<slug:slug>/', views.EventDetail.as_view(), name='event_detail'),
+    path('presence-go-event/<slug:slug>/', views.PresenceGoEvent.as_view(), name='presence_go_event'),
+    path('presence-maybe-event/<slug:slug>/', views.PresenceMaybeEvent.as_view(), name='presence_maybe_event'),
 ]
