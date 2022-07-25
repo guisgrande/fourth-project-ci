02922b54f40d319c9aa812fb5686d6125087ca81
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-25 21:03:12 +0000

    Add 2 new classes for presence view, add presence go and maybe to EventDetail class

--- events/views.py
+++ events/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.contrib.auth.models import User
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 from .models import Event
 from .forms import CommentForm
 
@@ -29,9 +30,21 @@ class EventDetail(View):
         queryset = Event.objects.filter(status=1)
         event = get_object_or_404(queryset, slug=slug)
         event_comments = event.event_comments.filter(approved=True).order_by("-created_on")
-    
+
+        # Presence confirm
+        presence_go_ok = False
+        presence_maybe_ok = False
+
+        if event.presence_go.filter(id=self.request.user.id).exists():
+            presence_go_ok = True
+
+        if event.presence_maybe.filter(id=self.request.user.id).exists():
+            presence_maybe_ok = True
+
         return render(request, "events/event_details.html", {
             "event": event,
+            "presence_go_ok": presence_go_ok,
+            "presence_maybe_ok": presence_maybe_ok,
             "event_comments": event_comments,
             "commented": False,
             "comment_form": CommentForm()
@@ -43,6 +56,7 @@ class EventDetail(View):
         event_comments = event.event_comments.filter(approved=True).order_by("-created_on")
         comment_form = CommentForm(data=request.POST)
 
+        # Comment form confirmation
         if comment_form.is_valid():
             comment_form.instance.name = request.user
             comment = comment_form.save(commit=False)
@@ -50,13 +64,67 @@ class EventDetail(View):
             comment.save()
         else:
             comment_form = CommentForm()
+
+        # Presence confirm
+        presence_go_ok = False
+        presence_maybe_ok = False
+
+        if event.presence_go.filter(id=self.request.user.id).exists():
+            presence_go_ok = True
+
+        if event.presence_maybe.filter(id=self.request.user.id).exists():
+            presence_maybe_ok = True
         
         return render(request, "events/event_details.html", {
             "event": event,
+            "presence_go_ok": presence_go_ok,
+            "presence_maybe_ok": presence_maybe_ok,
             "event_comments": event_comments,
             "commented": True,
             "comment_form": CommentForm()
             })
+
+
+class PresenceGoEvent(LoginRequiredMixin, View):
+    """
+    Class to logged user tag go an event.
+    """ 
+    def post(self, request, slug, *args, **kwargs):
+        event = get_object_or_404(Event, slug=slug)
+        go_filter = event.presence_go.filter(id=request.user.id)
+        maybe_filter = event.presence_maybe.filter(id=request.user.id)
+
+        if go_filter.exists():
+            event.presence_go.remove(request.user)
+        else:
+            event.presence_go.add(request.user)
+            if maybe_filter.exists():
+                event.presence_maybe.remove(request.user)
+            else:
+                pass
+
+        return HttpResponseRedirect(reverse('event_detail', args=[slug]))
+
+
+class PresenceMaybeEvent(LoginRequiredMixin, View):
+    """
+    Class to logged user tag maybe go an event.
+    """ 
+    def post(self, request, slug, *args, **kwargs):
+        event = get_object_or_404(Event, slug=slug)
+        go_filter = event.presence_go.filter(id=request.user.id)
+        maybe_filter = event.presence_maybe.filter(id=request.user.id)
+
+        if maybe_filter.exists():
+            event.presence_maybe.remove(request.user)
+        else:
+            event.presence_maybe.add(request.user)
+            if go_filter.exists():
+                event.presence_go.remove(request.user)
+            else:
+                pass
+
+        return HttpResponseRedirect(reverse('event_detail', args=[slug]))
 
 
 class AddEventPost(SuccessMessageMixin, LoginRequiredMixin, generic.CreateView):
