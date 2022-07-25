66ade7d529904558fb001e5d6081714155c2cd08
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-25 11:37:43 +0000

    Add RateForm to rate car post, with 5 fields to user review

--- garage/forms.py
+++ garage/forms.py
@@ -1,8 +1,27 @@
-from .models import CommentCar
+from .models import CommentCar, RateCar
 from django import forms
 
 
 class CommentForm(forms.ModelForm):
+    """
+    Form class to comment car post.
+    """
     class Meta:
         model = CommentCar
         fields = ('body',)
+
+
+class RateForm(forms.ModelForm):
+    """
+    Form class to rate car post.
+    """
+    class Meta:
+        model = RateCar
+        fields = ('price', 'aesthetics', 'speed', 'drivability', 'overall',)
+        labels = {
+            'price': ('Price: '),
+            'aesthetics': ('Aesthetics: '),
+            'speed': ('Speed: '),
+            'drivability': ('Drivability: '),
+            'overall': ('Overall: ')
+        }
