8c75359b8db26265a0270f0a553dc3ca43c5f459
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-28 17:49:43 +0000

    Add new path to search_car function view

--- garage/urls.py
+++ garage/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('car-detail/<slug:slug>/', views.CarDetail.as_view(), name='car_detail'),
     path('favourite/<slug:slug>/', views.FavouriteCar.as_view(), name='favourite_car'),
     path('rate-car/<slug:slug>/', views.RateCarView.as_view(), name='rate_car'),
+    path('search-car/', views.search_car, name='search_car'),
 ]
