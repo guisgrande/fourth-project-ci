b10afbfaf27f87fd978e02a1a95c7dea9efbdf66
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-07-28 15:56:53 +0000

    Edit path to events view, remove old and add new one

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('events/', views.EventsView.as_view(), name='events'),
+    path('events/', views.events, name='events'),
     path('add-event/', views.AddEventPost.as_view(), name='add_event'),
     path('edit-event/<slug:slug>/', views.EditEventPost.as_view(), name='edit_event'),
     path('delete-event/<slug:slug>/', views.DeleteEventPost.as_view(), name='delete_event'),
