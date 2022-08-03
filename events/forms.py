730d0de3641a9bd0302a37658f1d35faefd8f20f
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-08-03 08:09:18 +0000

    Add EventForm, Date and Time classes, to set input type to Event fields

--- events/forms.py
+++ events/forms.py
@@ -1,5 +1,32 @@
-from .models import CommentEvent
+from .models import Event, CommentEvent
 from django import forms
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class TimeInput(forms.DateInput):
+    input_type = 'time'
+
+
+class EventForm(forms.ModelForm):
+
+    class Meta:
+        model = Event
+        fields = [
+            'event_title',
+            'category',
+            'start_date',
+            'start_time',
+            'local',
+            'description',
+            'event_image',
+        ]
+        widgets = {
+            'start_date': DateInput(),
+            'start_time': TimeInput(),
+        }
 
 
 class CommentForm(forms.ModelForm):
