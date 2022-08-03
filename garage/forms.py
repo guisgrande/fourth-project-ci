6024307033e3b1477582d1131714381678eeac44
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-08-03 10:23:39 +0000

    Add CarForm to set placeholder and input type to Car fields

--- garage/forms.py
+++ garage/forms.py
@@ -1,5 +1,27 @@
-from .models import CommentCar, RateCar
+from .models import Car, CommentCar, RateCar
 from django import forms
+
+
+class CarForm(forms.ModelForm):
+    brand = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: Ford, Porsche, ...'}))
+    model = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ex: Mustang, 911, ...'}))
+    year = forms.IntegerField(widget=forms.NumberInput(attrs={'min': '1807', 'max': '2022', 'step':'1'}))
+    price = forms.FloatField(widget=forms.NumberInput(attrs={'min': '0'}))
+    hp = forms.FloatField(widget=forms.NumberInput(attrs={'min': '0'}))
+    speed = forms.FloatField(widget=forms.NumberInput(attrs={'min': '0'}))
+
+    class Meta:
+        model = Car
+        fields = [
+            'brand',
+            'model',
+            'year',
+            'price',
+            'hp',
+            'speed',
+            'description',
+            'car_image',
+        ]
 
 
 class CommentForm(forms.ModelForm):
