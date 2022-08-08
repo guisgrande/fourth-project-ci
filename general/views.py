819225ed4de1e8b18689086a2bc2a4514dd286bc
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-08-08 16:49:51 +0000

    Add limit to event list objects

--- general/views.py
+++ general/views.py
@@ -15,7 +15,7 @@ class IndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context.update({
-            'event_list': Event.objects.order_by('-created_on'),
+            'event_list': Event.objects.order_by('-created_on')[:4],
         })
         return context
 
